802247a0d39e05dbdf3325406323f0a66164d7c5
Gleidson <gleidsonrosasantos2@gmail.com>
2026-09-09 16:16:52 -0300

    migração de paginas

---
+++ OrderItem/src/app/presentation/pages/tela-inicial/tela-inicial.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-tela-inicial',
+  styleUrl: './tela-inicial.css',
+  templateUrl: './tela-inicial.html',
+})
+export class TelaInicial {}
