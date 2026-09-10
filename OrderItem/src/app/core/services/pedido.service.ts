5eb8143fd474fd562e262a3a998f4708afff1fe8
Luiz Kauã <lkaua4546@gmail.com>
2026-09-10 14:52:59 -0300

    Importado funções do angular,rxjs e model

--- OrderItem/src/app/core/services/pedido.service.ts
+++ OrderItem/src/app/core/services/pedido.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import{ Observable} from 'rxjs'
+import { Pedido } from '../domain/pedido.model';
 
 @Injectable({
   providedIn: 'root'
