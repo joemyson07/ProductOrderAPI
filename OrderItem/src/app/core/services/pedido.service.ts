3e77550d5cc7df0f2a29ec67e12147a389639ff1
Luiz Kauã <lkaua4546@gmail.com>
2026-09-10 16:02:48 -0300

    Removido uma / a mais do pedido

--- OrderItem/src/app/core/services/pedido.service.ts
+++ OrderItem/src/app/core/services/pedido.service.ts
@@ -12,23 +12,23 @@ export class PedidoService {
   private readonly apiBaseUrl = inject(API_BASE_URL);
 
   listar(): Observable<Pedido[]> {
-    return this.http.get<Pedido[]>(`${this.apiBaseUrl}/pedido/`)
+    return this.http.get<Pedido[]>(`${this.apiBaseUrl}/pedido`)
   }
 
   buscarPorId(idpedido:number): Observable<Pedido> {
     return this.http.get<Pedido>(`${this.apiBaseUrl}/pedido/${idpedido}`)
   }
 
-  dataPedido(data:Date | string): Observable<Pedido> {
+  dataPedido(data:Date | string): Observable<Pedido[]> {
     // Se for um objeto Date, gera "YYYY-MM-DDTHH:mm:ss", corta no 'T' e o [0] pega só a data ("YYYY-MM-DD")
     // Se já for string (texto), usa o valor direto sem alterar
     // Esse coméntario é para que, eu possa me lembrar depois como funciona  
     const dataInformada = data instanceof Date? data.toISOString().split('T')[0]:data 
 
-    return this.http.get<Pedido>(`${this.apiBaseUrl}/pedido/data/${dataInformada}`)
+    return this.http.get<Pedido[]>(`${this.apiBaseUrl}/pedido/data/${dataInformada}`)
   }
   
-  statusPedido(status:string):Observable<Pedido> {
-    return this.http.get<Pedido>(`${this.apiBaseUrl}/pedido/status/${status}`)
+  statusPedido(status:string):Observable<Pedido[]> {
+    return this.http.get<Pedido[]>(`${this.apiBaseUrl}/pedido/status/${status}`)
   }
 }
