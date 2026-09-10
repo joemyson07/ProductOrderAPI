ff14b0773aaf60026e111a2b36cceef91c725647
devguilhrm <guilimadev19@gmail.com>
2026-09-10 13:54:06 -0300

    iniciei ciração dos testes

--- OrderItem/src/app/core/services/produto.service.spec.ts
+++ OrderItem/src/app/core/services/produto.service.spec.ts
@@ -1,16 +1,32 @@
+import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { API_BASE_URL } from '../config/api.config';
 import { ProdutoService } from './produto.service';
 
 describe('ProdutoService', () => {
   let service: ProdutoService;
+  let http: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        { provide: API_BASE_URL, useValue: '/api'},
+      ],
+    });
+
     service = TestBed.inject(ProdutoService);
+    http: TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  afterEach(() => http.verify());
+
+  it('deve listar produtos', () => {
     expect(service).toBeTruthy();
   });
 });
