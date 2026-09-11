833eafe0cc400dd02b96cb29c7e5c15c61237e7c
Gleidson <gleidsonrosasantos2@gmail.com>
2026-09-11 14:23:00 -0300

    sessão 'Todos/Produtos' criada

--- OrderItem/src/app/presentation/pages/sessoes/todos/todos.component.ts
+++ OrderItem/src/app/presentation/pages/sessoes/todos/todos.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit {
 
+  produtos: any[] = [];
+
+  carregando = true;
+  erro = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.listarProdutos();
+  }
+
+  listarProdutos(): void {
+
+    this.http.get<any[]>('http://localhost:8000/produtos/')
+      .subscribe({
+
+        next: (dados) => {
+          this.produtos = dados;
+          this.carregando = false;
+        },
+
+        error: (erro) => {
+          console.error('Erro ao buscar produtos:', erro);
+          this.erro = 'Não foi possível carregar os produtos.';
+          this.carregando = false;
+        }
+
+      });
+  }
 }
